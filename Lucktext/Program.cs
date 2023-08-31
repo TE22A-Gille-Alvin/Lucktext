8cd045d48b673832238fd4e85119560bbdce985d
Alvin Gille <alvin.gille@elev.ga.ntig.se>
2023-08-31 10:40:16 +0200

    ljvyliyuv

---
+++ Lucktext/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("What is ur name?");
+string name = Console.ReadLine();
+Console.WriteLine("what is yums?");
+string name = Console.ReadLine();
+Console.WriteLine("how are u?");
+string name = Console.ReadLine();
+
+Console.WriteLine($"hey {name} cool name, mm {name} is indeed very yummers. u feel {name}? Thats unfortunate..");
+Console.ReadLine();
